9c6602bf946344b8a161cc71f3d7d29d924ec11d
Bruno Alliguieri <devalliguieri@gmail.com>
2026-05-24 21:06:51 -0300

    chore: scaffolding added

---
+++ cmd/cli/main.go
@@ -0,0 +1,39 @@
+package cli
+
+import (
+	"os"
+
+	tea "github.com/charmbracelet/bubbletea"
+	"github.com/spf13/cobra"
+)
+
+type SampleModel struct{}
+
+func (s SampleModel) Init() tea.Cmd {
+	panic("implement me")
+}
+
+func (s SampleModel) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
+	panic("implement me")
+}
+
+func (s SampleModel) View() string {
+	panic("implement me")
+}
+
+var rootCmd = &cobra.Command{
+	Use:   "",
+	Short: "Start Memory CLI",
+	RunE: func(cmd *cobra.Command, args []string) error {
+		p := tea.NewProgram(SampleModel{})
+
+		_, err := p.Run()
+		return err
+	},
+}
+
+func Execute() {
+	if err := rootCmd.Execute(); err != nil {
+		os.Exit(1)
+	}
+}
---
+++ main.go
@@ -0,0 +1,7 @@
+package main
+
+import "github.com/megalypse/memory_cli/cmd/cli"
+
+func main() {
+	cli.Execute()
+}
