d0552233298c6be61358a125d3cb5aa8fd0ae345
Bruno Alliguieri <devalliguieri@gmail.com>
2026-06-06 16:03:28 -0300

    chore: add memoryGroupId field to Memories struct

--- internal/views/memories.go
+++ internal/views/memories.go
@@ -1,24 +1,62 @@
 package views
 
 import (
+	"context"
+	"fmt"
 	tea "github.com/charmbracelet/bubbletea"
+	"github.com/megalypse/memory_cli/internal/memory"
 )
 
 type Memories struct {
 	memoryGroupId int
 	width, height int
+	memories      []*memory.Memory
+	repository    memory.Repository
+	err           error
+}
+
+func NewMemories(groupId int, repo memory.Repository) *Memories {
+	return &Memories{
+		memoryGroupId: groupId,
+		repository:    repo,
+	}
 }
 
 func (m *Memories) Init() tea.Cmd {
-	return nil
+	return m.loadMemories
 }
 
 func (m *Memories) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
-	return nil, nil
+	switch msg := msg.(type) {
+	case tea.KeyMsg:
+		switch msg.String() {
+		case "q", "esc":
+			return m, tea.Quit
+		}
+	}
+
+	return m, nil
 }
 
 func (m *Memories) View() string {
-	return ""
+	if m.err != nil {
+		return fmt.Sprintf("Error loading memories: %v", m.err)
+	}
+
+	if len(m.memories) == 0 {
+		return "No memories found for this group."
+	}
+
+	var result string
+	for i, memory := range m.memories {
+		result += fmt.Sprintf("[%d] %s\n", i+1, memory.Name)
+		if memory.Content != "" {
+			result += fmt.Sprintf("   %s\n", memory.Content)
+		}
+		result += "\n"
+	}
+
+	return result
 }
 
 func (m *Memories) SetSize(width, height int) {
@@ -32,4 +70,15 @@ func (m *Memories) GetWidth() int {
 
 func (m *Memories) GetHeight() int {
 	return m.height
+}
+
+func (m *Memories) loadMemories() tea.Msg {
+	memories, err := m.repository.GetAllByGroup(context.Background(), m.memoryGroupId)
+	if err != nil {
+		m.err = err
+		return nil
+	}
+
+	m.memories = memories
+	return nil
 }
